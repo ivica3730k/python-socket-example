e836680f2ba9a96e43f77c41b2604edd604c0e50
Ivica Matic <ivica.m@sundance.com>
2020-07-22 18:19:58 +0100

    Fix

--- opencv-client.py
+++ opencv-client.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import pickle
 import socket
 
@@ -21,12 +20,10 @@ client_socket.connect(('localhost', 8011))
 
 while True:
     try:
-        frame = recvall(client_socket)
-        frame = np.array(frame)
+        data = recvall(client_socket)
+        frame = pickle.loads(data, fix_imports=True, encoding="bytes")
         img = cv2.imdecode(frame, cv2.IMREAD_COLOR)
         cv2.imshow('Pic', img)
         cv2.waitKey(1)
     except pickle.UnpicklingError:
-        continue
-    except cv2.error:
         continue
--- opencv-server.py
+++ opencv-server.py
@@ -1,22 +1,23 @@
-import cv2
 import socket
+import pickle
+import cv2
 import time
 
-HOST = '0.0.0.0'
-PORT = 8011
+HOST='0.0.0.0'
+PORT=8011
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
+s.bind((HOST,PORT))
 s.listen(10)
 
 cam = cv2.VideoCapture(1)
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
 
-conn, addr = s.accept()
+conn,addr=s.accept()
 
 while True:
     ret, frameOriginal = cam.read()
     result, frame = cv2.imencode('.jpg', frameOriginal, encode_param)
-    conn.sendall(frame)
-    time.sleep(1/30)
+    data = pickle.dumps(frame,0)
+    conn.sendall(data)
